b1fe487bf570c2ff7eb4736465a9bea943789663
emad <emadabdalrahman11@gmail.com>
2017-04-07 22:35:36 +0200

    remove unused import

--- app/src/main/java/rofaeil/ashaiaa/idea/collegelife/MenuFragments/ChangePassword/ChangePasswordFragment.java
+++ app/src/main/java/rofaeil/ashaiaa/idea/collegelife/MenuFragments/ChangePassword/ChangePasswordFragment.java
@@ -14,7 +14,6 @@ import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
