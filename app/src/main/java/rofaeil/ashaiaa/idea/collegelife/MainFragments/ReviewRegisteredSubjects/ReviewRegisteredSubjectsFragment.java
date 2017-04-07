1920b5582b62c2fe1ff8ea70f73cddb0ec0d478e
emad <emadabdalrahman11@gmail.com>
2017-04-07 22:33:19 +0200

    remove unused import

--- app/src/main/java/rofaeil/ashaiaa/idea/collegelife/MainFragments/ReviewRegisteredSubjects/ReviewRegisteredSubjectsFragment.java
+++ app/src/main/java/rofaeil/ashaiaa/idea/collegelife/MainFragments/ReviewRegisteredSubjects/ReviewRegisteredSubjectsFragment.java
@@ -16,7 +16,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
