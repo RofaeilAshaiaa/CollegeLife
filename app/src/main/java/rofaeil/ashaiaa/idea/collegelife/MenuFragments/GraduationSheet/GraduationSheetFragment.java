aab146b6ff348d15b4f1ce89ceb6db29609547ce
emad <emadabdalrahman11@gmail.com>
2017-05-23 23:09:48 +0200

    remove parsing method from mean thread & update loader return to GraduationSheetData

--- app/src/main/java/rofaeil/ashaiaa/idea/collegelife/MenuFragments/GraduationSheet/GraduationSheetFragment.java
+++ app/src/main/java/rofaeil/ashaiaa/idea/collegelife/MenuFragments/GraduationSheet/GraduationSheetFragment.java
@@ -35,7 +35,7 @@ import static rofaeil.ashaiaa.idea.collegelife.Utils.FinalData.GRADUATION_SHEET_
 import static rofaeil.ashaiaa.idea.collegelife.Utils.StaticMethods.isNetworkAvailable;
 
 
-public class GraduationSheetFragment extends Fragment implements LoaderManager.LoaderCallbacks<Document> {
+public class GraduationSheetFragment extends Fragment implements LoaderManager.LoaderCallbacks<GraduationSheetData> {
 
     private View mRoot_view;
     private FragmentActivity mContext;
@@ -180,66 +180,22 @@ public class GraduationSheetFragment extends Fragment implements LoaderManager.L
 
     }
 
-    public GraduationSheetData getGraduationSheetData(Document document) {
-        GraduationSheetData mGraduationSheetData = new GraduationSheetData();
 
-        Element mSubjects_table = document.getElementById("ContentPlaceHolder1_ContentPlaceHolder1_ContentPlaceHolder1_GraduationSheetGridView");
-        Elements mSubjects_tr = mSubjects_table.getElementsByTag("tr");
-
-        ArrayList<GraduationSheetSubject> mSubjects = new ArrayList<>();
-        for (int i = 1; i < mSubjects_tr.size(); i++) {
-
-            GraduationSheetSubject mSubject = new GraduationSheetSubject();
-
-            mSubject.setCategory(mSubjects_tr.get(i).child(0).text());
-            mSubject.setID(mSubjects_tr.get(i).child(1).text());
-            mSubject.setOldID(mSubjects_tr.get(i).child(2).text());
-            mSubject.setName(mSubjects_tr.get(i).child(3).text());
-            mSubject.setHours(mSubjects_tr.get(i).child(4).text());
-            mSubject.setSemester(mSubjects_tr.get(i).child(5).text());
-            mSubject.setEarnedGrade(mSubjects_tr.get(i).child(6).text());
-            mSubject.setActualGrade(mSubjects_tr.get(i).child(7).text());
-            mSubject.setPoints(mSubjects_tr.get(i).child(8).text());
-            mSubject.setNodes(mSubjects_tr.get(i).child(9).text());
-            mSubject.setBackgroundId(new Random().nextInt(10));
-            mSubjects.add(mSubject);
-        }
-
-        Element mRequirement_table = document.getElementById("ContentPlaceHolder1_ContentPlaceHolder1_ContentPlaceHolder1_GraduationSheetSumGridView");
-        Elements mRequirement_tr = mRequirement_table.getElementsByTag("tr");
-
-        ArrayList<GraduationRequirements> mGraduationRequirements = new ArrayList<>();
-        for (int n = 1; n < mRequirement_tr.size(); n++) {
-
-            GraduationRequirements mRequirement = new GraduationRequirements();
-
-            mRequirement.setName(mRequirement_tr.get(n).child(0).text());
-            mRequirement.setRequiredHours(mRequirement_tr.get(n).child(1).text());
-            mRequirement.setEarnedHours(mRequirement_tr.get(n).child(2).text());
-
-            mGraduationRequirements.add(mRequirement);
-        }
-
-        mGraduationSheetData.setGraduationRequirements(mGraduationRequirements);
-        mGraduationSheetData.setSubject(mSubjects);
-
-        return mGraduationSheetData;
-    }
 
     @Override
-    public Loader<Document> onCreateLoader(int id, Bundle args) {
+    public Loader<GraduationSheetData> onCreateLoader(int id, Bundle args) {
         return new AsyncTaskLoaderGraduationSheet(mContext);
     }
 
     @Override
-    public void onLoadFinished(Loader<Document> loader, Document data) {
-        mGraduationSheetData = getGraduationSheetData(data);
+    public void onLoadFinished(Loader<GraduationSheetData> loader, GraduationSheetData data) {
+        mGraduationSheetData = data;
         initializeRecycleView();
         mProgressDialog.dismiss();
     }
 
     @Override
-    public void onLoaderReset(Loader<Document> loader) {
+    public void onLoaderReset(Loader<GraduationSheetData> loader) {
 
     }
 
