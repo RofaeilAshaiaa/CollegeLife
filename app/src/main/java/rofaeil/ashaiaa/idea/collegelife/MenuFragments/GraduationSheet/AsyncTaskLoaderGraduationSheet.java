83b28c439399585d841aa9f94171e72d3ba1ecf8
emad <emadabdalrahman11@gmail.com>
2017-05-26 02:35:29 +0200

    update loader to return Connection.Response

--- app/src/main/java/rofaeil/ashaiaa/idea/collegelife/MenuFragments/GraduationSheet/AsyncTaskLoaderGraduationSheet.java
+++ app/src/main/java/rofaeil/ashaiaa/idea/collegelife/MenuFragments/GraduationSheet/AsyncTaskLoaderGraduationSheet.java
@@ -23,14 +23,14 @@ import static rofaeil.ashaiaa.idea.collegelife.Utils.FinalData.GraduationSheetUR
 /**
  * Created by emad on 1/1/2017.
  */
-public class AsyncTaskLoaderGraduationSheet extends AsyncTaskLoader<GraduationSheetData> {
+public class AsyncTaskLoaderGraduationSheet extends AsyncTaskLoader<Connection.Response> {
 
     public AsyncTaskLoaderGraduationSheet(Context context) {
         super(context);
     }
 
     @Override
-    public GraduationSheetData loadInBackground() {
+    public Connection.Response loadInBackground() {
         try {
 
             Connection.Response mGraduation_sheet_response = Jsoup.connect(GraduationSheetURL)
@@ -39,9 +39,7 @@ public class AsyncTaskLoaderGraduationSheet extends AsyncTaskLoader<GraduationSh
                     .cookies(MainActivity.mapLoginPageCookies)
                     .execute();
 
-            Document mGraduationSheetDocument = mGraduation_sheet_response.parse();
-
-            return getGraduationSheetData(mGraduationSheetDocument);
+            return mGraduation_sheet_response;
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -49,50 +47,6 @@ public class AsyncTaskLoaderGraduationSheet extends AsyncTaskLoader<GraduationSh
         return null;
     }
 
-    public GraduationSheetData getGraduationSheetData(Document document) {
-        GraduationSheetData mGraduationSheetData = new GraduationSheetData();
 
-        Element mSubjects_table = document.getElementById("ContentPlaceHolder1_ContentPlaceHolder1_ContentPlaceHolder1_GraduationSheetGridView");
-        Elements mSubjects_tr = mSubjects_table.getElementsByTag("tr");
-
-        ArrayList<GraduationSheetSubject> mSubjects = new ArrayList<>();
-        for (int i = 1; i < mSubjects_tr.size(); i++) {
-
-            GraduationSheetSubject mSubject = new GraduationSheetSubject();
-
-            mSubject.setCategory(mSubjects_tr.get(i).child(0).text());
-            mSubject.setID(mSubjects_tr.get(i).child(1).text());
-            mSubject.setOldID(mSubjects_tr.get(i).child(2).text());
-            mSubject.setName(mSubjects_tr.get(i).child(3).text());
-            mSubject.setHours(mSubjects_tr.get(i).child(4).text());
-            mSubject.setSemester(mSubjects_tr.get(i).child(5).text());
-            mSubject.setEarnedGrade(mSubjects_tr.get(i).child(6).text());
-            mSubject.setActualGrade(mSubjects_tr.get(i).child(7).text());
-            mSubject.setPoints(mSubjects_tr.get(i).child(8).text());
-            mSubject.setNodes(mSubjects_tr.get(i).child(9).text());
-            mSubject.setBackgroundId(new Random().nextInt(10));
-            mSubjects.add(mSubject);
-        }
-
-        Element mRequirement_table = document.getElementById("ContentPlaceHolder1_ContentPlaceHolder1_ContentPlaceHolder1_GraduationSheetSumGridView");
-        Elements mRequirement_tr = mRequirement_table.getElementsByTag("tr");
-
-        ArrayList<GraduationRequirements> mGraduationRequirements = new ArrayList<>();
-        for (int n = 1; n < mRequirement_tr.size(); n++) {
-
-            GraduationRequirements mRequirement = new GraduationRequirements();
-
-            mRequirement.setName(mRequirement_tr.get(n).child(0).text());
-            mRequirement.setRequiredHours(mRequirement_tr.get(n).child(1).text());
-            mRequirement.setEarnedHours(mRequirement_tr.get(n).child(2).text());
-
-            mGraduationRequirements.add(mRequirement);
-        }
-
-        mGraduationSheetData.setGraduationRequirements(mGraduationRequirements);
-        mGraduationSheetData.setSubject(mSubjects);
-
-        return mGraduationSheetData;
-    }
 
 }
